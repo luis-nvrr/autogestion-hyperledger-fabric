da32733f8b4f2de8234464c0a154264bc343a746
Luis Navarro <luisfnvrr@gmail.com>
2022-03-28 13:22:59 -0300

    feat: add script to enroll new users

---
+++ autogestion/application-gateway-go/enrollUser.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/hyperledger/fabric-sdk-go/pkg/gateway"
+)
+
+const (
+	org1MSP        = "Org1MSP"
+	org3MSP        = "Org3MSP"
+	org1HostName   = "org1.example.com"
+	org3HostName   = "org3.example.com"
+	org1User       = "User1@org1.example.com"
+	org3User       = "User3@org3.example.com"
+	teachersWallet = "teachers-wallet"
+	studentsWallet = "students-wallet"
+)
+
+var now = time.Now()
+
+func main() {
+	log.Println("============ enrollTeacher application-golang starts ============")
+
+	org := os.Args[1]
+	userId := os.Args[2]
+
+	switch org {
+	case "org1":
+		err := addUserToWallet(userId, org1MSP, org1HostName, org1User, teachersWallet)
+		if err != nil {
+			log.Println(err)
+		}
+	case "org3":
+		err := addUserToWallet(userId, org3MSP, org3HostName, org3User, studentsWallet)
+		if err != nil {
+			log.Println(err)
+		}
+	default:
+		log.Println("invalid organization")
+	}
+}
+
+func addUserToWallet(user, orgMSP, orgHostName, orgUser, walletName string) error {
+	fmt.Printf("\n--> Register and enrolling new user")
+
+	err := os.Setenv("DISCOVERY_AS_LOCALHOST", "true")
+	if err != nil {
+		log.Fatalf("Error setting DISCOVERY_AS_LOCALHOST environemnt variable: %v", err)
+	}
+
+	wallet, err := gateway.NewFileSystemWallet(walletName)
+	if err != nil {
+		log.Fatalf("Failed to create wallet: %v", err)
+	}
+
+	if wallet.Exists(user) {
+		return fmt.Errorf("user %s already exists", user)
+	}
+
+	credPath := filepath.Join(
+		"..",
+		"..",
+		"test-network",
+		"organizations",
+		"peerOrganizations",
+		orgHostName,
+		"users",
+		orgUser,
+		"msp",
+	)
+
+	certPath := filepath.Join(credPath, "signcerts", orgUser+"-"+"cert.pem")
+	cert, err := ioutil.ReadFile(filepath.Clean(certPath))
+	if err != nil {
+		return err
+	}
+
+	keyDir := filepath.Join(credPath, "keystore")
+	files, err := ioutil.ReadDir(keyDir)
+	if err != nil {
+		return err
+	}
+	if len(files) != 1 {
+		return fmt.Errorf("keystore folder should have contain one file")
+	}
+
+	keyPath := filepath.Join(keyDir, files[0].Name())
+	key, err := ioutil.ReadFile(filepath.Clean(keyPath))
+	if err != nil {
+		return err
+	}
+
+	identity := gateway.NewX509Identity(orgMSP, string(cert), string(key))
+
+	return wallet.Put(user, identity)
+}
---
+++ autogestion/application-gateway-go/go.mod
@@ -0,0 +1,5 @@
+module enrollUser
+
+go 1.16
+
+require github.com/hyperledger/fabric-sdk-go v1.0.0
