750c228207fe3e1dc757fcf5eb5d7fec4a4bf4df
Luis Navarro <luisfnvrr@gmail.com>
2022-03-29 02:10:26 -0300

    fix: typo in CA path & remove unused files

--- test-network/setOrgEnv.sh
+++ test-network/setOrgEnv.sh
@@ -35,6 +35,13 @@ elif [[ ${ORG,,} == "org2" || ${ORG,,} == "magnetocorp" ]]; then
    CORE_PEER_ADDRESS=localhost:9051
    CORE_PEER_TLS_ROOTCERT_FILE=${DIR}/test-network/organizations/peerOrganizations/org2.example.com/tlsca/tlsca.org1.example.com-cert.pem
 
+elif [[ ${ORG,,} == "org3" || ${ORG,,} == "magnetocorp" ]]; then
+
+   CORE_PEER_LOCALMSPID=Org3MSP
+   CORE_PEER_MSPCONFIGPATH=${DIR}/test-network/organizations/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp
+   CORE_PEER_ADDRESS=localhost:11051
+   CORE_PEER_TLS_ROOTCERT_FILE=${DIR}/test-network/organizations/peerOrganizations/org3.example.com/tlsca/tlsca.org1.example.com-cert.pem
+
 else
    echo "Unknown \"$ORG\", please choose Org1/Digibank or Org2/Magnetocorp"
    echo "For example to get the environment variables to set upa Org2 shell environment run:  ./setOrgEnv.sh Org2"
